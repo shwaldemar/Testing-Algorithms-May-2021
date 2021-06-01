0f984bd0a04ba94900bc914c01125502be62760b
Simon Hall <37545481+shwaldemar@users.noreply.github.com>
2021-06-01 12:52:16 +0100

    added data test function & tests & called to affirm progress.

--- stringMutation.js
+++ stringMutation.js
@@ -11,8 +11,23 @@ Lastly, ['Alien', 'line'], should return true because all of the letters in 'lin
 //Tests
 const test1 = ['hello', 'Hello'];
 const test2 = ['hello', 'hey'];
-const test2 = ['Alien', 'line'];
+const test3 = ['Alien', 'line'];
 
 const mutationOfString = (stringArray) => {
-
+  let target = stringArray[0].toLowerCase();
+  let test = stringArray[1].toLowerCase();
+  let result = [target, test];
+  return result;
 };
+
+//test data
+const algDataTest = () => {
+  let dataTest1Result = mutationOfString(test1);
+  let dataTest2Result = mutationOfString(test2);
+  let dataTest3Result = mutationOfString(test3);
+  let results = [dataTest1Result, dataTest2Result, dataTest3Result];
+  return results;
+};
+
+//call test algDataTest.
+console.log(algDataTest(test1, test2, test3));
